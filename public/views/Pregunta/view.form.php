90fe7d9816544e20683d15f694e6fefe801fe2c6
Jane <lajane2020@hotmail.com>
2017-08-30 23:31:34 -0500

    Inclusión de validaciones

--- public/views/Pregunta/view.form.php
+++ public/views/Pregunta/view.form.php
@@ -29,7 +29,7 @@ $(document).ready(function() {
 									message: 'El Nombre no puede ser vacío.'
 								},
 								regexp: {
-									regexp: /^[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9\.\,\_\s\-]+$/,
+									regexp: /^[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9\.\,\_\s\-\(\)]+$/,
 									message: 'Ingrese un Nombre válido.'
 								}
 							}
@@ -38,7 +38,7 @@ $(document).ready(function() {
 					message: 'La Descripción no es válida',
 					validators: {
 								regexp: {
-									regexp: /^[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9\.\,\_\s\-]+$/,
+									regexp: /^[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9\.\,\_\s\-\(\)]+$/,
 									message: 'Ingrese una Descripción válida.'
 								}
 							}
