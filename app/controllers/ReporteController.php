739ce610871921e05e9cad2f4cdd20d61378de6b
Jane <lajane2020@hotmail.com>
2017-09-07 23:38:12 -0500

    Cambios

--- app/controllers/ReporteController.php
+++ app/controllers/ReporteController.php
@@ -78,223 +78,228 @@ class ReporteController {
 		$datos_cab = self::obtenerDatosCab($id)[0];
 		$respuestas = self::obtenerRespuestas();
 		$preguntas = self::obtenerPreguntas($id);
-		
-		$html = "<html>
-					<head>
-						<link href='http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css' rel='stylesheet'/>
-						<style>
-								body {
-								margin: 20px 20px 20px 50px;
+		if(count($preguntas) >0){		
+			$html = "<html>
+						<head>
+							<link href='http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css' rel='stylesheet'/>
+							<style>
+									body {
+									margin: 20px 20px 20px 50px;
+									}
+									table{
+									border-collapse: collapse; width: 100%;
+									}
+									
+									td{
+									border:1px solid #ccc; padding:1px;
+									font-size:9pt;
+									}
+							</style>
+						</head>
+						<body>
+							<table width= 100% border=0 >
+								<tr>
+									<td style='text-align:left' rowspan=2>
+										<img src='".PATH_IMAGE."/san_gabriel.jpg' style='height: 80px; margin-bottom: 5px;'>
+									</td>
+									<td style='font-size:20px;text-align:left' colspan='5'>
+										<b>INSTITUTO TECNOLÓGICO SUPERIOR SAN GABRIEL</b>
+									</td>										
+								</tr>					
+								<tr>
+									<td style='font-size:14px;text-align:center' colspan='6'><b>Evaluación Docentes ".$datos_cab->periodo_nombre."</b></td>
+								</tr>
+							</table>
+							<br>
+							<table border=0>				
+								<tr>
+									<td width='60px'><b>Área:</b></td><td style='text-align:left'>".$datos_cab->espe_nombre."</td>
+									<td width='60px'><b>Sección:</b></td style='text-align:left'><td>".$datos_cab->seccion_nombre."</td>
+									<td width='60px'><b>Materia:</b></td style='text-align:left'><td>".$datos_cab->materia_nombre."</td>
+								</tr>			
+								<tr>
+									<td width='60px'><b>Docente:</b></td><td style='text-align:left'>".$datos_cab->docente_nombre." ".$datos_cab->docente_apellido."</td>
+									<td width='60px'><b>Curso:</b></td><td style='text-align:left'>".$datos_cab->curso_nombre."</td>								
+									<td width='150px'><b>Fecha de Evaluación:</b></td><td style='text-align:left'>".$preguntas[0]->fecha_evaluacion."</td>
+								</tr>
+							</table>
+							<br>
+							<table>
+								<tr>
+									<td rowspan='3' style='text-align:center'><b>Preguntas</b>
+									</td>										
+									<td colspan='4' style='text-align:center'><b>Valor del Grado de Acuerdo con las siguientes afirmaciones</b></td>
+									<td rowspan='3' style='text-align:center'><b>Total Alumnos</b>
+								</tr>
+								<tr>";		
+								foreach ($respuestas as $res){
+									$html.="<td style='text-align:center'><b>".$res->nombre."</b></td>";							
 								}
-								table{
-								border-collapse: collapse; width: 100%;
+			$html .="	</tr>";
+								foreach ($respuestas as $res){
+									$html.="<td style='text-align:center'><b>".$res->valor."</b></td>";
 								}
-								
-								td{
-								border:1px solid #ccc; padding:1px;
-								font-size:9pt;
-								}
-						</style>
-					</head>
-					<body>
+			$html .="	</tr>	";
+			$total = 0;
+			$total1 = 0;
+			$total2 = 0;
+			$total3 = 0;
+			$total4 = 0;
+								foreach ($preguntas as $preg){
+									if($preg->unica == 0){
+			$total_preg = $preg->res1+$preg->res2+$preg->res3+$preg->res4;
+			$total += $total_preg;
+			$total1 += $preg->res1;
+			$total2 += $preg->res2;
+			$total3 += $preg->res3;
+			$total4 += $preg->res4;
+			
+			$html .="	<tr><td>".$preg->pregunta_nombre."</td>";								
+			$html .="		<td style='text-align:center'>".$preg->res1."</td>";							
+			$html .="		<td style='text-align:center'>".$preg->res2."</td>";								
+			$html .="		<td style='text-align:center'>".$preg->res3."</td>";							
+			$html .="		<td style='text-align:center'>".$preg->res4."</td>";		
+			$html .="       <td style='text-align:center'>".$total_preg."</td></tr>";
+						}
+					}
+			$perc[] = number_format((($total1 *100)/$total),2);
+			$perc[] = number_format((($total2 *100)/$total),2);
+			$perc[] = number_format((($total3 *100)/$total),2);
+			$perc[] = number_format((($total4 *100)/$total),2);
+						
+			$html .="<tr><td style='text-align:center'><b>TOTAL</b></td>
+						 <td style='text-align:center'>".$total1."</td>
+						 <td style='text-align:center'>".$total2."</td>
+						 <td style='text-align:center'>".$total3."</td>
+						 <td style='text-align:center'>".$total4."</td>
+						 <td style='text-align:center'>".$total."</td>
+					</tr>
+					<tr>
+					   	<td></td>
+						<td style='text-align:center'>".$perc[0]."</td>
+						<td style='text-align:center'>".$perc[1]."</td>
+						<td style='text-align:center'>".$perc[2]."</td>
+						<td style='text-align:center'>".$perc[3]."</td>
+						<td style='text-align:center'>100</td>
+					</tr>";
+			$total1 = 0;
+			$total1a = 0;
+			$total2b = 0;
+			$total3c = 0;
+			$total4d = 0;
+			$html .="<tr><td colspan='6'><br><br></td></tr>";
+			foreach ($preguntas as $preg){
+				if($preg->unica == 1){
+					$total_preg1 = $preg->res1+$preg->res2+$preg->res3+$preg->res4;
+					$total1 += $total_preg;
+					$total1a += $preg->res1;
+					$total2b += $preg->res2;
+					$total3c += $preg->res3;
+					$total4d += $preg->res4;
+			
+					$html .="	<tr><td>".$preg->pregunta_nombre."</td>";
+					$html .="		<td style='text-align:center'>".$preg->res1."</td>";
+					$html .="		<td style='text-align:center'>".$preg->res2."</td>";
+					$html .="		<td style='text-align:center'>".$preg->res3."</td>";
+					$html .="		<td style='text-align:center'>".$preg->res4."</td>";
+					$html .="       <td style='text-align:center'>".$total_preg1."</td></tr>";
+				}
+			}
+			
+			$perc1[] = number_format((($total1a *100)/$total1),2);
+			$perc1[] = number_format((($total2b *100)/$total1),2);
+			$perc1[] = number_format((($total3c *100)/$total1),2);
+			$perc1[] = number_format((($total4d *100)/$total1),2);
+			
+			$html .="<tr>
+					   	<td></td>
+						<td style='text-align:center'>".$perc1[0]."</td>
+						<td style='text-align:center'>".$perc1[1]."</td>
+						<td style='text-align:center'>".$perc1[2]."</td>
+						<td style='text-align:center'>".$perc1[3]."</td>
+						<td style='text-align:center'>100</td>
+					</tr>";
+			$html .="</table>";
+			self::grafico($perc,1);
+			self::grafico($perc1,2);
+			
+			$html.="<br>
+					<br>
+					<div style='page-break-after:always;'></div>		
+						<table width= 100% border=0>
+							<tr>
+								<td style='text-align:left:width:10%'>
+									<img src='".PATH_IMAGE."/san_gabriel.jpg' style='height: 80px; margin-bottom: 5px;'>
+								<td style='text-align:center'>			
+									<span style='text-align:center;font-size:20px'><b>Análisis e Interpretación de Resultados</b></span>
+								</td>										
+							</tr>
+						</table>
 						<table width= 100% border=0 >
 							<tr>
-								<td style='text-align:left' rowspan=2>
-									<img src='".PATH_IMAGE."/san_gabriel.jpg' style='height: 80px; margin-bottom: 5px;'>
+								<td align='center'>
+											<span style='text-align:center;font-size:15px'><b>Gestión del Docente</b></span><br>
 								</td>
-								<td style='font-size:20px;text-align:left' colspan='5'>
-									<b>INSTITUTO TECNOLÓGICO SUPERIOR SAN GABRIEL</b>
-								</td>										
-							</tr>					
-							<tr>
-								<td style='font-size:14px;text-align:center' colspan='6'><b>Evaluación Docentes ".$datos_cab->periodo_nombre."</b></td>
-							</tr>
-						</table>
-						<br>
-						<table border=0>				
-							<tr>
-								<td width='60px'><b>Área:</b></td><td style='text-align:left'>".$datos_cab->espe_nombre."</td>
-								<td width='60px'><b>Sección:</b></td style='text-align:left'><td>".$datos_cab->seccion_nombre."</td>
-								<td width='60px'><b>Materia:</b></td style='text-align:left'><td>".$datos_cab->materia_nombre."</td>
 							</tr>			
 							<tr>
-								<td width='60px'><b>Docente:</b></td><td style='text-align:left'>".$datos_cab->docente_nombre." ".$datos_cab->docente_apellido."</td>
-								<td width='60px'><b>Curso:</b></td><td style='text-align:left'>".$datos_cab->curso_nombre."</td>								
-								<td width='150px'><b>Fecha de Evaluación:</b></td><td style='text-align:left'>".$preguntas[0]->fecha_evaluacion."</td>
+								<td align='justify'>
+											Entre la población estudiantil encuestada se encontraron los siguiente datos: Por un "
+											.$perc[3]."% que está totalmente deacuerdo con la gestión del docente, un "
+											.$perc[2]."% que está deacuerdo, un "
+											.$perc[1]."% que está en desacuerdo y un "										
+											.$perc[0]."% que está totalmente desacuerdo.
+								</td>
 							</tr>
-						</table>
-						<br>
-						<table>
 							<tr>
-								<td rowspan='3' style='text-align:center'><b>Preguntas</b>
-								</td>										
-								<td colspan='4' style='text-align:center'><b>Valor del Grado de Acuerdo con las siguientes afirmaciones</b></td>
-								<td rowspan='3' style='text-align:center'><b>Total Alumnos</b>
+								<td align='center'>
+									<img src=".PATH_IMAGE."/graficas/imagen.png>
+								</td>
 							</tr>
-							<tr>";		
-							foreach ($respuestas as $res){
-								$html.="<td style='text-align:center'><b>".$res->nombre."</b></td>";							
+							<tr>
+								<td align='center'>
+											<span style='text-align:center;font-size:15px'><b>Gestión del Docente</b></span><br>
+								</td>
+							</tr>			
+							<tr>
+								<td align='justify'>
+											Entre la población estudiantil encuestada se encontraron los siguiente datos: Por ";
+							if($perc1[3] >0){
+								$html.=	" un ".$perc1[3]."% que está totalmente deacuerdo que el docente nunca falto a clases";
 							}
-		$html .="	</tr>";
-							foreach ($respuestas as $res){
-								$html.="<td style='text-align:center'><b>".$res->valor."</b></td>";
+							if($perc1[2] >0){
+								$html.= " un ".$perc1[2]."% que esta deacuerdo que el docente falto a clases";
 							}
-		$html .="	</tr>	";
-		$total = 0;
-		$total1 = 0;
-		$total2 = 0;
-		$total3 = 0;
-		$total4 = 0;
-							foreach ($preguntas as $preg){
-								if($preg->unica == 0){
-		$total_preg = $preg->res1+$preg->res2+$preg->res3+$preg->res4;
-		$total += $total_preg;
-		$total1 += $preg->res1;
-		$total2 += $preg->res2;
-		$total3 += $preg->res3;
-		$total4 += $preg->res4;
-		
-		$html .="	<tr><td>".$preg->pregunta_nombre."</td>";								
-		$html .="		<td style='text-align:center'>".$preg->res1."</td>";							
-		$html .="		<td style='text-align:center'>".$preg->res2."</td>";								
-		$html .="		<td style='text-align:center'>".$preg->res3."</td>";							
-		$html .="		<td style='text-align:center'>".$preg->res4."</td>";		
-		$html .="       <td style='text-align:center'>".$total_preg."</td></tr>";
-					}
-				}
-		$perc[] = number_format((($total1 *100)/$total),2);
-		$perc[] = number_format((($total2 *100)/$total),2);
-		$perc[] = number_format((($total3 *100)/$total),2);
-		$perc[] = number_format((($total4 *100)/$total),2);
-					
-		$html .="<tr><td style='text-align:center'><b>TOTAL</b></td>
-					 <td style='text-align:center'>".$total1."</td>
-					 <td style='text-align:center'>".$total2."</td>
-					 <td style='text-align:center'>".$total3."</td>
-					 <td style='text-align:center'>".$total4."</td>
-					 <td style='text-align:center'>".$total."</td>
-				</tr>
-				<tr>
-				   	<td></td>
-					<td style='text-align:center'>".$perc[0]."</td>
-					<td style='text-align:center'>".$perc[1]."</td>
-					<td style='text-align:center'>".$perc[2]."</td>
-					<td style='text-align:center'>".$perc[3]."</td>
-					<td style='text-align:center'>100</td>
-				</tr>";
-		$total1 = 0;
-		$total1a = 0;
-		$total2b = 0;
-		$total3c = 0;
-		$total4d = 0;
-		$html .="<tr><td colspan='6'><br><br></td></tr>";
-		foreach ($preguntas as $preg){
-			if($preg->unica == 1){
-				$total_preg1 = $preg->res1+$preg->res2+$preg->res3+$preg->res4;
-				$total1 += $total_preg;
-				$total1a += $preg->res1;
-				$total2b += $preg->res2;
-				$total3c += $preg->res3;
-				$total4d += $preg->res4;
-		
-				$html .="	<tr><td>".$preg->pregunta_nombre."</td>";
-				$html .="		<td style='text-align:center'>".$preg->res1."</td>";
-				$html .="		<td style='text-align:center'>".$preg->res2."</td>";
-				$html .="		<td style='text-align:center'>".$preg->res3."</td>";
-				$html .="		<td style='text-align:center'>".$preg->res4."</td>";
-				$html .="       <td style='text-align:center'>".$total_preg1."</td></tr>";
-			}
+							if($perc1[1] >0){
+								$html.= " un ".$perc1[1]."% que está en desacuerdo que el docente falto a clases";
+							}
+							if($perc1[0] >0){
+								$html.= " un ".$perc1[1]."% que el docente nunca asistió a clases.";
+							}
+																					
+						$html.="</td>
+							</tr>
+							<tr>
+								<td align='center'>								
+									<img src=".PATH_IMAGE."/graficas/imagen1.png>
+								</td>
+							</tr>
+						</table>	
+					</body></html>";
+			
+			$options = new Options();
+			$options->set('isHtml5ParserEnabled', true);
+			$dompdf = new Dompdf($options);
+			$dompdf->load_html($html);
+			
+			$dompdf->render();
+			$canvas = $dompdf->get_canvas();
+			$canvas->page_text(550, 750, "{PAGE_NUM}", $font, 6, array(0,0,0)); //header		
+			$dompdf->stream('general', array("Attachment"=>false));		
 		}
-		
-		$perc1[] = number_format((($total1a *100)/$total1),2);
-		$perc1[] = number_format((($total2b *100)/$total1),2);
-		$perc1[] = number_format((($total3c *100)/$total1),2);
-		$perc1[] = number_format((($total4d *100)/$total1),2);
-		
-		$html .="<tr>
-				   	<td></td>
-					<td style='text-align:center'>".$perc1[0]."</td>
-					<td style='text-align:center'>".$perc1[1]."</td>
-					<td style='text-align:center'>".$perc1[2]."</td>
-					<td style='text-align:center'>".$perc1[3]."</td>
-					<td style='text-align:center'>100</td>
-				</tr>";
-		$html .="</table>";
-		self::grafico($perc,1);
-		self::grafico($perc1,2);
-		
-		$html.="<br>
-				<br>
-				<div style='page-break-after:always;'></div>		
-					<table width= 100% border=0>
-						<tr>
-							<td style='text-align:left:width:10%'>
-								<img src='".PATH_IMAGE."/san_gabriel.jpg' style='height: 80px; margin-bottom: 5px;'>
-							<td style='text-align:center'>			
-								<span style='text-align:center;font-size:20px'><b>Análisis e Interpretación de Resultados</b></span>
-							</td>										
-						</tr>
-					</table>
-					<table width= 100% border=0 >
-						<tr>
-							<td align='center'>
-										<span style='text-align:center;font-size:15px'><b>Gestión del Docente</b></span><br>
-							</td>
-						</tr>			
-						<tr>
-							<td align='justify'>
-										Entre la población estudiantil encuestada se encontraron los siguiente datos: Por un "
-										.$perc[3]."% que está totalmente deacuerdo con la gestión del docente, un "
-										.$perc[2]."% que está deacuerdo, un "
-										.$perc[1]."% que está en desacuerdo y un "										
-										.$perc[0]."% que está totalmente desacuerdo.
-							</td>
-						</tr>
-						<tr>
-							<td align='center'>
-								<img src=".PATH_IMAGE."/graficas/imagen.png>
-							</td>
-						</tr>
-						<tr>
-							<td align='center'>
-										<span style='text-align:center;font-size:15px'><b>Gestión del Docente</b></span><br>
-							</td>
-						</tr>			
-						<tr>
-							<td align='justify'>
-										Entre la población estudiantil encuestada se encontraron los siguiente datos: Por ";
-						if($perc1[3] >0){
-							$html.=	" un ".$perc1[3]."% que está totalmente deacuerdo que el docente nunca falto a clases";
-						}
-						if($perc1[2] >0){
-							$html.= " un ".$perc1[2]."% que esta deacuerdo que el docente falto a clases";
-						}
-						if($perc1[1] >0){
-							$html.= " un ".$perc1[1]."% que está en desacuerdo que el docente falto a clases";
-						}
-						if($perc1[0] >0){
-							$html.= " un ".$perc1[1]."% que el docente nunca asistió a clases.";
-						}
-																				
-					$html.="</td>
-						</tr>
-						<tr>
-							<td align='center'>								
-								<img src=".PATH_IMAGE."/graficas/imagen1.png>
-							</td>
-						</tr>
-					</table>	
-				</body></html>";
-		
-		$options = new Options();
-		$options->set('isHtml5ParserEnabled', true);
-		$dompdf = new Dompdf($options);
-		$dompdf->load_html($html);
-		
-		$dompdf->render();
-		$canvas = $dompdf->get_canvas();
-		$canvas->page_text(550, 750, "{PAGE_NUM}", $font, 6, array(0,0,0)); //header		
-		$dompdf->stream('general', array("Attachment"=>false));		
+		else{
+			$_SESSION ['message'] = "No existen datos registrados.";
+			header ( "Location: ../listar/" );
+		}
 	}
 	
 	public function grafico($datos,$imagen){
